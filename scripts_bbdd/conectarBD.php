36451814b924b2758e9816061292af6ec79472fe
BorjaRM <borjaroyuelamesa@gmail.com>
2017-05-28 02:14:09 +0200

    ip

--- scripts_bbdd/conectarBD.php
+++ scripts_bbdd/conectarBD.php
@@ -1,7 +1,7 @@
 <?php
 
 //Creamos la conexion con la base de datos
-$conectar = mysqli_connect("52.90.200.239","usuario","Pintegrado2017","hotel_pi");
+$conectar = mysqli_connect("localhost","usuario","Pintegrado2017","hotel_pi");
 if (!$conectar){
 	// En caso de que no se pueda conectar, se finaliza la conexión.
 	echo ("Fallo al conectar a Mysql: ".mysqli_connect_errno($conectar)." ".mysqli_connect_error($conectar)."<br>");
